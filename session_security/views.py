47a2ffe37ce92adcff5ccebe215b15abf337a2b1
Eddie Cohen <31940792+edcohen08@users.noreply.github.com>
2023-01-19 18:23:55 -0500

    Feature/alpinejs (#1)
    * if hitting the ping then update last activity
    
    * typo
    
    * trying to get inactive for
    
    * rename template
    
    * make it a html file
    
    * switch to html in view
    
    * back to before
    
    * reset middleware
    
    * remove extra quote
    
    * delete ping template
    
    * undo unnecc deletion

--- session_security/views.py
+++ session_security/views.py
@@ -21,7 +21,7 @@ class PingView(generic.View):
     def get(self, request, *args, **kwargs):
         if '_session_security' not in request.session:
             # It probably has expired already
-            return http.HttpResponse('"logout"',
+            return http.HttpResponse("logout",
                                      content_type='application/json')
 
         last_activity = get_last_activity(request.session)
