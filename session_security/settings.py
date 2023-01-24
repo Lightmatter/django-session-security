3509be813bffdaee12ebbbe47808f6d89c9a182f
Eddie Cohen <eddie@lightmatter.com>
2023-01-24 12:38:36 -0500

    middleware not working

--- session_security/settings.py
+++ session_security/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     "django.contrib.messages"
 ]
 
-MIDDLEWARE = ["django.contrib.sessions.middleware.SessionMiddleware", "django.contrib.messages.middleware.MessageMiddleware"]
+MIDDLEWARE = getattr(settings, "MIDDLEWARE")
 
 TEMPLATES = getattr(settings, "TEMPLATES")
 
